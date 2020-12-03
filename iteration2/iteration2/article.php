d1d84c9a7dc4a1f8ec0e19031ed9c33a964ee73e
AhmedHamma <73638564+AhmedHamma@users.noreply.github.com>
2020-12-03 11:20:33 +0100

    Add files via upload
    ajout du fichier functions

---
+++ iteration2/iteration2/article.php
@@ -0,0 +1,23 @@
+<?php
+$articles = [
+    [
+        "nom" => "voiture rouge",
+        "prix" => " 1500euro",
+        "photo" => "voiturerouge.jpg",
+        "alt" => "voiturerouge"
+    ],
+    [
+        "nom" => "voiture noire",
+        "prix" => " 1500euro",
+        "photo" => "voiturenoir.jpg ",
+        "alt" => "voiturenoire"
+    ],
+    [
+        "nom" => "voiture blanche",
+        "prix" => " 1500euro",
+        "photo" => "voitureblanche.jpg",
+        "alt"=> "voitureblanche"
+    ]
+];
+
+?>
